54cb2ebfdaf892b31065d79d60c166d64ab166df
sandervspl <sandervispoel@hotmail.com>
2021-05-03 20:29:33 +0200

    Simplify server to dev-server

--- client/server/index.ts
+++ client/server/index.ts
@@ -1,54 +1,26 @@
-import path from 'path';
 import { parse } from 'url';
+import { createServer } from 'http';
 import next from 'next';
-import express from 'express';
-import { PORT, NODE_ENV } from '../config/env';
+
+import { PORT, NODE_ENV, APP_ENV } from '../config/env';
 
 const app = next({
-  dev: NODE_ENV === 'development',
+  dev: true,
 });
 
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  const server = express();
-
-  const staticFiles = [
-    {
-      path: '/service-worker.js',
-      // Don't cache service worker is a best practice
-      // Clients wont get emergency bug fixes etc.
-      cache: false,
-    },
-    {
-      path: '/robots.txt',
-      cache: true,
-    },
-  ];
-
-  // Handle static file routes
-  for (const file of staticFiles) {
-    server.get(file.path, (req, res) => {
-      const filePath = path.resolve(`dist/static${file.path}`);
-
-      if (!file.cache) {
-        res.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-      }
-
-      app.serveStatic(req, res, filePath);
+  createServer((req, res) => {
+    if (req.url) {
+      // parse is deprecated, but the WHATWG URL API does not comply with the handle API
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    }
+  })
+    .listen(PORT, () => {
+      console.info(`[${NODE_ENV} / ${APP_ENV}] Server running on http://localhost:${PORT}`);
     });
-  }
-
-  // Handle other routes
-  server.get('*', (req, res) => {
-    // parse is deprecated, but the WHATWG URL API does not comply with the handle API
-    const parsedUrl = parse(req.url, true);
-    handle(req, res, parsedUrl);
-  });
-
-  server.listen(PORT, () => {
-    console.info(`[${NODE_ENV}] Server running on http://localhost:${PORT}`);
-  });
 })
   .catch((err) => {
     console.error(err.stack);
